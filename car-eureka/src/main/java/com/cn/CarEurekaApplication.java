13496cc0fcf5515ce0547c2c71e4cdd548899aa8
lwjGitzy <135552143159@163.com>
2019-05-15 21:42:16 +0800

    lwj -买车的项目（框架），第一次提交.

--- car-eureka/src/main/java/com/cn/CarEurekaApplication.java
+++ car-eureka/src/main/java/com/cn/CarEurekaApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer  //代表开启eureka的注册中心服务
 public class CarEurekaApplication {
 
-    //
+    //34567s
     public static void main(String[] args) {
         SpringApplication.run(CarEurekaApplication.class, args);
     }
