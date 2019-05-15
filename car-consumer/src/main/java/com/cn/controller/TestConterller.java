fab673aaf932ca76604eafaf2002f04fa5e0dada
lyn <13051982369.com>
2019-05-15 21:19:36 +0800

    分支lyn

---
+++ car-consumer/src/main/java/com/cn/controller/TestConterller.java
@@ -0,0 +1,25 @@
+package com.cn.controller;
+
+import com.cn.model.TestBean;
+import com.cn.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/test")
+@RestController
+public class TestConterller {
+
+    @Autowired
+    private TestService testService;
+
+    @GetMapping("/queryTest")
+    public List<TestBean> queryTest(){
+        System.out.println("查");
+        List<TestBean> list= testService.queryTest();
+        return list;
+    };
+}
