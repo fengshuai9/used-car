9d5190c82159978b863d354e507dbac04faf8e13
fengshuai9 <1848123832@qq.com>
2019-05-20 18:14:26 +0800

    第二次提交ce

--- car-apis/src/main/java/com/cn/model/TestBean.java
+++ car-apis/src/main/java/com/cn/model/TestBean.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Data
 public class TestBean {
 
-    //主键id  合并主干测试haha
+    //主键id  合并主干测试haha2222
     private  Integer id;
     private  String name;
     private String info;
